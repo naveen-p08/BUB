70b57748bc69026ba913235b47c0f87cd71c079b
Naveen <naveen1808@live.in>
2023-07-25 13:46:46 +0530

    deployment v1

---
+++ controllers/comments.js
@@ -0,0 +1,20 @@
+const Comments = require("../models/Comments");
+
+module.exports = {
+
+    createComment: async (req, res) => {
+        try {
+
+            await Comments.create({
+                comment: req.body.comment,
+                likes: 0,
+                post: req.params.id,
+            });
+            console.log("comment has been added!");
+            res.redirect("/post/" + req.params.id);
+        } catch (err) {
+            console.log(err);
+        }
+    },
+
+};
